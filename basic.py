1322d338a248c0f029483adaf41efc3a8ce98704
ethan <ethan.locke@gmail.com>
2014-10-09 12:51:26 -0600

    semi functioning class

--- basic.py
+++ basic.py
@@ -2,10 +2,7 @@ from sympy import *
 
 class Equation(object):
     # dimcon_dict items convert keys to base units when multiplied
-    length = {'ft': 0.3048}
-    force = {'lbs': 0.2248}
-    energy = {'ft*lbs': 0.7376}
-    dimcon_dicts = [length, force, energy]
+    dimcon_dict = {'ft': 0.3048, 'lbs': 4.4482, 'ft*lbs': 1.3558}
     in_dict = {}
     def __init__(self, equation, eq_dict):
         self.equation = equation
@@ -26,8 +23,7 @@ class Equation(object):
             if key is not get:
                 known_val = int(input("Value of {}: ".format(key)))
                 known_units = input("Units of {}: ".format(key))
-                dim_type =
-                known_data = ([known_val, known_units], dim_type)
+                known_data = [known_val, known_units]
                 self.in_dict[key]= known_data
         #print(self.in_dict)
         return self.in_dict
@@ -36,21 +32,17 @@ class Equation(object):
         # all units made dimensionally consistent
         # checks if unit is in dimcon_dict. if not prints "Conversion not Stored"
         # compares in_dict[key][0] ro dimcon_dict[key]
-        #for key in self.in_dict:
-            #if self.in_dict[key][1] in self.dimcon_dict:
-                #print(self.dimcon_dict[self.in_dict[key][1]])
-                #self.in_dict[key][0] = self.in_dict[key][0]*self.dimcon_dict[self.in_dict[key][1]]
+        for key in self.in_dict:
+            if self.in_dict[key][1] in self.dimcon_dict:
+                self.in_dict[key][0] = self.in_dict[key][0]*self.dimcon_dict[self.in_dict[key][1]]
+            else:
+                print('Conversion not stored')
                 # need to change units to base units somehow
                 # elements need way to auto-recognize their base unit type
                 # perhaps dimcon method can just activate an element method
                 # that tells ele's to change value to correspond to a unittobase conversion
                 # self.in_dict[key][1] = self.dimcon_dict[self.in_dict[key][1]]
-        for key in self.in_dict:
-            # for every variable in in_dict
-            if
-            # search dimcon_dicts for str(dim_type)
-            item for item in self.dimcon_dicts if item[]
-        print(self.in_dict)
+        # print(self.in_dict)
         # print(self.equation)
         # sym = symbol(str(sym))
 
