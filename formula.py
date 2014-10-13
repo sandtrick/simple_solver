181181e55adab5e8a9ef03e7ff9fd5c9cf0e51d0
ethan <ethan.locke@gmail.com>
2014-10-13 14:47:49 -0600

    law of cosines test

--- formula.py
+++ formula.py
@@ -95,14 +95,26 @@ class Formula(object):
             print(answer)
             return answer
 
+# initialize new formula
+a, b, c, A = symbols('a b c A')
+law_of_cos_eq = Eq(a**2, b**2+c**2-2*b*c*cos(A))
+#pprint(law_of_cos_eq, use_unicode=True)
+law_of_cos_dict = {'a': (a, 'm'), 'b': (b, 'm'),
+ 'c': (c,'m'), 'A': (A, 'radians')}
+law_of_cos = Formula(law_of_cos_eq, law_of_cos_dict)
+
+# solve it
+law_of_cos.solvefor(a, 'ft')
+
+
 ##### Soon these will be function to speed process further
 # creates symbols for equation
-x, F, W = symbols('x F W')
+#x, F, W = symbols('x F W')
 # makes equation
-work_eq = Eq(x*F, W)
+#work_eq = Eq(x*F, W)
 # dictionary attaches base-units to symbols
-work_dict = {'x': (x, 'm'), 'F': (F, 'N'), 'W': (W, 'N*m')}
+#work_dict = {'x': (x, 'm'), 'F': (F, 'N'), 'W': (W, 'N*m')}
 # equation and units linked together in Equation class
-work = Formula(work_eq, work_dict)
+#work = Formula(work_eq, work_dict)
 # solves equation for specified variable in desired units
-work.solvefor(W, 'J')
+#work.solvefor(x, 'yard')
