693d34f43f445f02371f4e2c20c43206f1b1539e
ethan <ethan.locke@gmail.com>
2014-10-14 22:34:21 -0600

    testing new solvefor method and quick entry ideas

--- formula.py
+++ formula.py
@@ -77,10 +77,11 @@ class Formula(object):
             self.clear_inputs()
             self.input_vars(get)
             self.dimcon()
-            get = str(get)
+            get = Symbol(get)
             # get_units = str(get_units)
             # solve for get
             arrange = solve(self.equation, get)
+            get = str(get)
             # sympy sub to substitute dimcon.dimcon_dict into symfor's eq
             sub_vals = []
             for key in self.eq_dict:
@@ -94,39 +95,3 @@ class Formula(object):
             #answer = base_sol[0]/self.dimcon.dimcon_dict[get_units]
             print(answer)
             return answer
-
-'''
-# initialize new formula
-a, b, c, A = symbols('a b c A')
-law_of_cos_eq = Eq(a**2, (b**2)+(c**2)-(2*b*c*cos(A)))
-#pprint(law_of_cos_eq, use_unicode=True)
-law_of_cos_dict = {'a': (a, 'm'), 'b': (b, 'm'),
- 'c': (c,'m'), 'A': (A, 'radians')}
-law_of_cos = Formula(law_of_cos_eq, law_of_cos_dict)
-
-# solve it
-law_of_cos.solvefor(a, 'm')
-'''
-
-'''
-##### Soon these will be function to speed process further
-# creates symbols for equation
-x, F, W = symbols('x F W')
-# makes equation
-work_eq = Eq(x*F, W)
-# dictionary attaches base-units to symbols
-work_dict = {'x': (x, 'm'), 'F': (F, 'N'), 'W': (W, 'N*m')}
-# equation and units linked together in Equation class
-work = Formula(work_eq, work_dict)
-# solves equation for specified variable in desired units
-#work.solvefor(x, 'yard')
-'''
-
-'''
-K, m , v = symbols('K m v')
-kin_eq = Eq(K, 0.5*m*v**2)
-kin_dict = {'K': (K, 'J'), 'm': (m, 'kg'), 'v': (v, 'm/s')}
-kin = Formula(kin_eq, kin_dict)
-
-kin.solvefor(m, 'kg')
-'''
